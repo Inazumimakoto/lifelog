d871a84504f7fd095bc2e2b40e4e8829e1a2a3f0
inazumimakoto <inazumimakoto@gmail.com>
2025-11-13 14:26:58 +0900

    Initial Commit

---
+++ lifelog/lifelogApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifelogApp.swift
+//  lifelog
+//
+//  Created by inazumimakoto on 2025/11/13.
+//
+
+import SwiftUI
+
+@main
+struct lifelogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
